24403fa9008d481061649cf824195ac689d7e12e
KimSungHun <kimtiger1201@naver.com>
2023-06-29 04:14:07 +0900

    TArray<>

--- CryptRaider/TriggerComponent.cpp
+++ CryptRaider/TriggerComponent.cpp
@@ -3,9 +3,26 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
 void UTriggerComponent::BeginPlay()
 {
     Super::BeginPlay();
+}
 
-    UE_LOG(LogTemp, Display, TEXT("Your message"));
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+
+    if(Actors.Num() > 0){
+        FString ActorName = Actors[0]->GetActorNameOrLabel();
+        UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+    } 
+
 }
